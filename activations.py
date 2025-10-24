a83c18c16721180e6cf775cc84e5ebceade2a1af
Alexander Pichner <ajpichner@gmail.com>
2025-10-24 17:52:29 +0100

    relu as a function

--- activations.py
+++ activations.py
@@ -1,5 +1,6 @@
 inputs = []
-output = []
 
-for input in inputs:
-    output.append(max(0, input))
+def ReLU(inputs):
+    output = []
+    for input in inputs:
+        output.append(max(0, input))
