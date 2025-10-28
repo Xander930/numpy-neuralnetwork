de285ea497f7a8fe378775d731a21696deec9da9
Alexander Pichner <ajpichner@gmail.com>
2025-10-28 20:46:15 +0000

    softmax implementation

--- source/activations.py
+++ source/activations.py
@@ -5,3 +5,12 @@ class ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
+
+def softmax(x):
+    exp_vals = []
+    norm_vals = []
+    for output in x:
+        exp_vals.append(np.exp(output))
+    norm_div = sum(exp_vals)
+    for val in exp_vals:
+        norm_vals.append(val / norm_div)
