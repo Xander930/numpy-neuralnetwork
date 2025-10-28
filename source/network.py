b33130cd19e5cad355e77e7a12f2912381be1546
Alexander Pichner <ajpichner@gmail.com>
2025-10-28 13:55:56 +0000

    network.py implementation and first layer creation using created data

--- source/network.py
+++ source/network.py
@@ -3,11 +3,12 @@ from activations import ReLU
 from data import create_data
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 X, y = create_data(100, 3)
 
 layer1 = Layer(2, 5)
 activation1 = ReLU()
 
-#layer1.forward(X)
-#activation1.forward(layer1.output)
+layer1.forward(X)
+activation1.forward(layer1.output)
