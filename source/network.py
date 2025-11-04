91af4bebd246714847433e1b3fc367ee7678d98c
Alexander Pichner <ajpichner@gmail.com>
2025-11-04 14:56:53 +0000

    final organization to utils and full forward network test

--- source/network.py
+++ source/network.py
@@ -1,16 +1,21 @@
-from layer import Layer
-from activations import ReLU
 from data import create_data
-
-import matplotlib.pyplot as plt
-import numpy as np
+from utils import CCE, Layer, ReLU, Softmax
 
 X, y = create_data(100, 3)
 
-layer1 = Layer(2, 5)
-activation1 = ReLU()
+layer1 = Layer(2, 3)
+layer2 = Layer(3, 3)
+
+activ1 = ReLU()
+activ2 = Softmax()
 
 layer1.forward(X)
+activ1.forward(layer1.output)
 
-activation1.forward(layer1.output)
-print(activation1.output)
+layer2.forward(activ1.output)
+activ2.forward(layer1.output)
+
+loss_f = CCE()
+loss = loss_f.calculate(activ2.output, y)
+
+print("Loss:", loss)
--- source/utils.py
+++ source/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Layer:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -19,3 +20,24 @@ class Softmax:
         exp_vals = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
         probs = exp_vals / np.sum(exp_vals, axis=1, keepdims=True)
         self.output = probs
+
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        batch_loss = np.mean(sample_losses)
+        return batch_loss
+
+
+class CCE(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clip = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            conf_corrects = y_pred_clip[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            conf_corrects = np.sum(y_pred_clip * y_true, axis=1)
+
+        neg_log_probs = -np.log(conf_corrects)
+        return neg_log_probs
