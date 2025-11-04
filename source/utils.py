a37e82c922bb1b3df66e3347185a0eac75588fd5
Alexander Pichner <ajpichner@gmail.com>
2025-11-04 13:49:31 +0000

    reorganization of modules to src/utils for simplicity

--- source/utils.py
+++ source/utils.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+class Layer:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.bias = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, np.array(self.weights)) + self.bias
+
 
 class ReLU:
     def forward(self, inputs):
