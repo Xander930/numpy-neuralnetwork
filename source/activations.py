1541ebb961350f0d39a0017e428c206cbcc7d8fb
Alexander Pichner <ajpichner@gmail.com>
2025-11-07 22:03:35 +0000

    bugfix: remove np.clip

--- source/activations.py
+++ source/activations.py
@@ -2,9 +2,8 @@ import numpy as np
 
 
 def sigmoid(x):
-    return 1.0 / (1.0 + np.exp(-np.clip(x, -500, 500)))
+    return 1.0 / (1.0 + np.exp(-x))
 
-
-def sigmoid_prime(x):
+def  sigmoid_prime(x):
     s = sigmoid(x)
     return s * (1 - s)
