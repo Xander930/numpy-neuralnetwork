6e6c1070963298ac8b670e68a064eceb96ef94bb
Alexander Pichner <ajpichner@gmail.com>
2025-11-07 23:03:52 +0000

    activation function docstrings

--- source/activations.py
+++ source/activations.py
@@ -2,9 +2,15 @@ import numpy as np
 
 
 def sigmoid(x):
+    """
+    sigmoid activation function: inputs np.ndarray and returns np.ndarray
+    """
     return 1.0 / (1.0 + np.exp(-x))
 
 
 def sigmoid_prime(x):
+    """
+    derivative of the sigmoid function, input ndarray and returns np.ndarray
+    """
     s = sigmoid(x)
     return s * (1 - s)
