0d27c9d34835cb121ffd42b5c39f9d566675bbea
Alexander Pichner <ajpichner@gmail.com>
2025-11-10 15:16:21 +0000

    loss function and layer class docstrings

--- source/utils.py
+++ source/utils.py
@@ -2,6 +2,20 @@ import numpy as np
 
 
 class Layer:
+    """
+    general layer class
+
+    3 variables in cache:
+    - self.weights, weight matrix for layer cached during __init__
+    - self.bias, bias matrix for layer cached during __init__
+
+    -self.output, layer output matrix cached during forward pass
+
+    __init__ method: specifcy input matrix size and number of neurons in layer
+
+    forward method: pass inputs through layer
+    """
+
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
         self.bias = np.zeros((1, n_neurons))
@@ -12,8 +26,14 @@ class Layer:
 
 
 def loss_mse(y, y_pred):
+    """
+    mean squared error loss function, inputs 2x np,ndarray and returns float
+    """
     return np.mean((y - y_pred) ** 2)
 
 
 def mse_prime(y, y_pred):
+    """
+    derivative of MSE function, inputs 2x np.ndarray and returns float
+    """
     return 2 * (y_pred - y)
